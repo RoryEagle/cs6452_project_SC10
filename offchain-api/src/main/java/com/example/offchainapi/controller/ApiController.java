de011972ce6613b5cb7d9f1420a1fc6e99fcaaa4
Matthew <matthew.qian2@gmail.com>
2022-07-23 13:49:21 +1000

    add tree integration

--- offchain-api/src/main/java/com/example/offchainapi/controller/ApiController.java
+++ offchain-api/src/main/java/com/example/offchainapi/controller/ApiController.java
@@ -4,6 +4,7 @@ import com.example.offchainapi.models.Tree;
 import com.example.offchainapi.service.ApplicationService;
 import net.minidev.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,5 +26,16 @@ public class ApiController {
         JSONObject object = new JSONObject();
         object.put("temperature", "10");
         return ResponseEntity.ok(object);
+    }
+
+    @PostMapping("/addTree")
+    public ResponseEntity<String> addTree(@RequestBody Tree tree) {
+        try {
+            System.out.println("received! " + tree.getLocation() + tree.getAddress() + tree.getOwner());
+            service.addTree(tree);
+            return ResponseEntity.status(HttpStatus.CREATED).build();
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("invalid inputs");
+        }
     }
 }
--- offchain-api/src/main/java/com/example/offchainapi/models/Tree.java
+++ offchain-api/src/main/java/com/example/offchainapi/models/Tree.java
@@ -1,14 +1,24 @@
 package com.example.offchainapi.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NonNull;
 
 import javax.persistence.*;
 
 @Data
 @Entity
 @Table
+@AllArgsConstructor
 public class Tree {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    String id;
+    @NonNull
+    String address;
+    @NonNull
+    String owner;
+    @NonNull
+    String location;
+
+    public Tree() {
+    }
 }
