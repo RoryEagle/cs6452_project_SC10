79f1f5caccc902bac6d252ef9eeb531f8e582d6c
HYH-MATTHEW <108671139+HYH-MATTHEW@users.noreply.github.com>
2022-08-07 08:53:03 +0800

    Update CarbonCredit.sol
    Adding documentation and check expired

--- CarbonCredit.sol
+++ CarbonCredit.sol
@@ -20,40 +20,57 @@ contract CarbonCredit {
         forSale = false;
         expired = false;
     }
-
-    function buy() public returns (address, address payable, uint256) {
+    
+    /// @notice Check if this Carbon Credit is for Sale and identity of buyer
+    /// @dev oldOwnerRegistryAddr saved and pass back to ownerRegistry for transaction,
+             ownerRegistry should check if oldOwnerRegistry is updated before transaction and changeOwner.
+    /// @return oldOwnerRegistryAddr, owner and salePrice for transaction if check is true, 
+                msg.sender instead of oldOwnerRegistryAddr if check is false. 
+    function buy() public returns (address, address payable, uint256) notExpire {
         address oldOwnerRegistryAddr = ownerRegistryAddr;
         if (forSale == true && ownerRegistryAddr != msg.sender) {
             return (oldOwnerRegistryAddr, owner, salePrice);
         }
         return (msg.sender, owner, salePrice);
     }
-
-    function changeOwner (address payable newOwner) public {
+    
+    /// @ notice change owner of this Carbon Credit and reset forSale
+    /// @ dev should only be called if buy() returns the correct address
+    /// @ param newOwner address of the new owner of this Carbon Credit
+    function changeOwner (address payable newOwner) public notExpire {
         console.log('sender', msg.sender);
         address oldOwnerRegistryAddr = ownerRegistryAddr;
         ownerRegistryAddr = msg.sender;
         owner = newOwner;
         forSale = false;
     }
-
+    
+    /// @ notice change forSale to true and setup the salePrice
+    /// @ param price salePrice of this Carbon Credit
     function sell (uint256 price) public restricted returns (bool) {
         forSale = true;
         salePrice = price;
         return forSale;
     }
-
+    
+    /// @ notice use the Carbon Credit(i.e set expired to true)
     function use() public restricted returns (bool) {
         if (expired) {
             return false;
         }
-        console.log('Im here', payable(address(this)));
         expired = true;
         return true;
     }
-
+    
+    /// @notice Check if this Carbon Credit is expired
+    modifier notExpire() {
+        require (expired == false, "This Carbon Credit has already been used");
+        _;
+    }
+    
+    /// @notice Only owner can do
     modifier restricted() {
-        require (msg.sender == ownerRegistryAddr, "Can only be executed by the owner of this carbonCredit");
+        require (msg.sender == ownerRegistryAddr , "Can only be executed by the owner of this carbonCredit");
         _;
     }
 }
