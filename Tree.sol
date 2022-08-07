62a33c23d0384d3684989aba708705cd1638cdc5
richard5118085 <108671229+richard5118085@users.noreply.github.com>
2022-08-07 13:09:37 +1000

    added comments and removed console.log lines

--- Tree.sol
+++ Tree.sol
@@ -25,26 +25,30 @@ contract Tree {
         treeType = tree_type;
         CO2 = 250;
         plantDate = block.timestamp;
-        console.log("tree loc output is: ", location);
-        console.log("Tree owner: ", owner);
-        console.log("Tree creator: ", creator);
         forSale = false;
         
     }
-
+    
+    ///@notice Gets the location of the tree
+    ///@return tree location
     function getTreeLocation() public returns (string memory) {
         return location;
     }
 
+    ///@notice Used to check if tree is verified.
+    ///@return verified boolean
     function isVerified() public returns (bool) {
         return verified;
     }
-
+    
+    ///@notice Gets the CO2 of the tree
+    ///@return CO2 amount
     function getCO2() public returns (uint256) {
         return CO2;
     }
 
-
+    ///@notice Get information required to buy a tree
+    ///@return Registry address, owner address, sale price.
     function buy() public returns (address, address payable, uint256) {
         address oldOwnerRegistryAddr = ownerRegistryAddr;
         if (forSale == true && ownerRegistryAddr != msg.sender) {
@@ -52,17 +56,19 @@ contract Tree {
         }
         return (msg.sender, owner, salePrice);
     }
-
+    
+    ///@notice Change the owner of the tree after buying
+    ///@param New owner address
     function changeOwner (address payable newOwner) public {
-        console.log('msg sender', msg.sender);
         address oldOwnerRegistryAddr = ownerRegistryAddr;
-
         ownerRegistryAddr = msg.sender;
         owner = newOwner;
         forSale = false;
-
     }
-
+    
+    ///@notice Change the required values when selling 
+    ///@param price the tree is selling for
+    ///@return changed forSale boolean
     function sell(uint256 price) public restricted returns (bool) {
         forSale = true;
         salePrice = price;
@@ -70,27 +76,32 @@ contract Tree {
         return forSale;
     }
     
+    ///@notice Change the verifired value to true
     function verifyTree() public {
-        console.log("Made it into the verifyTree tree function");
         verified = true;
     }
 
+    ///@notice Reduce CO2 value by the amount used. Available CO2 must be more than or equaled to amount used
+    ///@param CO2 amountUsud
     function useCO2(uint256 amountUsed) public restricted {
         require (CO2 >= amountUsed, "CO2 used is more than the available amount");
         CO2 = CO2 - amountUsed;
     }
 
-    // returns age in seconds
+    ///@notice Get the age of the tree
+    ///@return age in seconds
     function getAge() public restricted returns (uint256) {
         uint256 age = block.timestamp - plantDate;
         return age;
     }
 
+    ///@notice Use all but the amount of CO2 specified in the remainder
+    ///@param remainder CO2 amount 
     function useAllButSomeCO2(uint256 remainder) public restricted {
         require (CO2 >= remainder, "CO2 used is more than the available amount");
         CO2 = remainder;
     }
-
+    ///@notice Use all the CO2 by setting the CO2 to 0
     function useAllCO2() public restricted {
         CO2 = 0;
     }
